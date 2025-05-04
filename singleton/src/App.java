44c3901542b8b740669960d0039dd36bcda4be00
Marcelo Alves <161260493+marcelodev30@users.noreply.github.com>
2025-05-04 21:25:06 +0000

    .....

--- singleton/src/App.java
+++ singleton/src/App.java
@@ -6,7 +6,7 @@ public class App {
         configManager.setModeEscuro(true);
         configManager.setIdioma("pt-br");
 
-        ConfigManager newConfigManager =ConfigManager.getInstancia();
-        newConfigManager.PrintConfiguracoes();
+        ConfigManager newConfig =ConfigManager.getInstancia();
+        newConfig.PrintConfiguracoes();
     }
 }
